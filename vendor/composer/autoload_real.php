c7c1bb6d789e89384b4c73463fa5d8524f35b91b
Gabriel Brandão <deostulti2@gmail.com>
2024-10-22 00:20:23 +0100

    Workflow Test

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit28fbef08330f004494f4c15d9a0f8155
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit28fbef08330f004494f4c15d9a0f8155', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit28fbef08330f004494f4c15d9a0f8155', 'loadClassLoader'));
